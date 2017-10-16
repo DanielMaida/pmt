27c46557ec21a77a5f130e8ff70f34e4c5fefd22
Arthur Freitas <abf2@cin.ufpe.br>
2017-10-16 00:03:29 -0300

    Add sellers header

---
+++ src/sellers.h
@@ -0,0 +1,12 @@
+#ifndef SELLERS_H
+#define SELLERS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+int callSellers(std::string pattern, std::vector<std::string> textFiles, int patternIsPatternPath, int printOnlyCount, int error);
+
+#endif
